92cb86c0aac2ee24e51ee7c856c3801cb50a814d
Matt Hauser <matt.hauser.web@gmail.com>
2023-04-10 15:01:06 -0700

    removed moves from opp players turn

--- src/components/BattleApp.tsx
+++ src/components/BattleApp.tsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { RootState } from "../app/store";
 import { useEffect } from "react";
 import { fetchPokemon } from "../slices/pokemonSlice";
@@ -69,33 +69,43 @@ const HealthBar: React.FC<HealthBarProps> = ({ currentHP, originalHP }) => {
 const BattleApp: React.FC<PokemonProps> = () => {
   const dispatch = useDispatch();
 
-  const [imageAnimationProps, setImageAnimationProps] = useSpring(() => ({
-    x: 0,
-  }));
-
   const [p1HP, setP1HP] = useState<number>(0);
   const [p2HP, setP2HP] = useState<number>(0);
   const [playerTurn, setPlayerTurn] = useState<boolean>(true);
   const [stats1, setStats1] = useState<Stats>(pokemonSeed[0].stats);
   const [stats2, setStats2] = useState<Stats>(pokemonSeed[2].stats);
   const [lastMoveUsed, setLastMoveUsed] = useState<Move | null>(null);
-
+  const [hideButtons, setHideButtons] = useState(false);
   const initialTurnRecord = new Map<number, Turn>();
   const [turnRecord, setTurnRecord] = useState(initialTurnRecord);
 
-  const [isLeft, setIsLeft] = React.useState(false);
+  const [attacked, setAttacked] = useState(false);
+
+  const isAninamting = useRef(false);
   //janky animation
   const springProps = useSpring({
     to: async (next, cancel) => {
-      await next({ left: isLeft ? "calc(0% - 20px)" : "calc(0% + 30px)" });
+      await next({ left: "calc(0% - 30px)" });
       await next({ left: "calc(0% - 10px)" });
       await next({ left: "calc(0% - 15px)" });
-
       await next({ left: "0" });
-      setIsLeft(!isLeft);
     },
     from: { left: "0%" },
     config: { tension: 300, friction: 10 },
+  });
+
+  const props = useSpring({
+    to: async (next) => {
+      await next(attacked ? { marginLeft: -30 } : { marginLeft: 0 }); // slide to the left over 1 second
+      await next(
+        attacked
+          ? { marginLeft: 50, config: { duration: 100 } }
+          : { marginLeft: 0 }
+      ); // jump back to the right by 50 pixels and bounce slightly over 0.5 seconds
+      await next(attacked ? { marginLeft: -10 } : { marginLeft: 0 }); // return to starting position over 1 second
+    },
+    from: { marginLeft: -10 },
+    config: { tension: 300, friction: 20 }, // adjust these values for the desired animation effect
   });
 
   //   useEffect(() => {
@@ -129,12 +139,15 @@ const BattleApp: React.FC<PokemonProps> = () => {
     const newTurnRecord = new Map(
       Array.from(turnRecord).concat([[numTurns, newTurn]])
     );
+
     setTurnRecord(newTurnRecord);
   }
   // this click handler does basically all of the logic.
   const handleMoveClick = (move: Move, source: string) => {
     let attackerStats = source === "p1" ? stats1 : stats2;
     let defenderStats = source === "p1" ? stats2 : stats1;
+
+    setAttacked(true);
     //crit rate
     const criticalHitRate = (attackerStats.speed * 100) / 512;
     const isCriticalHit = Math.random() * 100 <= criticalHitRate + 5;
@@ -233,43 +246,47 @@ const BattleApp: React.FC<PokemonProps> = () => {
     };
     // conditional damage application logic.
     if (source === "p1") {
+      setHideButtons(true);
+      setTimeout(() => setHideButtons(false), 3000);
       let conDMG = 0;
       if (stats2.hasCondition && stats2.condition === "leech") {
         const damageFraction = 1 / 16;
         conDMG = Math.floor(defenderStats.hp * damageFraction + 1);
       }
 
-      setTimeout(() => setP2HP(hp - conDMG), 3000);
+      setTimeout(() => setP2HP(hp - conDMG), 2500);
       if (p2HP < 0) {
-        setTimeout(() => setP2HP(0), 3000);
+        setTimeout(() => setP2HP(0), 2500);
       }
       if (p1HP > pokemonSeed[0].stats.hp) {
-        setTimeout(() => setP1HP(pokemonSeed[0].stats.hp), 3000);
+        setTimeout(() => setP1HP(pokemonSeed[0].stats.hp), 2500);
       } else {
-        setTimeout(() => setP1HP(p1HP + conDMG), 3000);
+        setTimeout(() => setP1HP(p1HP + conDMG), 2500);
       }
 
       setLastMoveUsed(move);
       handleAddTurn(pokemonSeed[0].name, pokemonSeed[2].name, move);
-      setTimeout(() => setPlayerTurn(false), 4000);
+      setTimeout(() => setPlayerTurn(false), 3000);
     } else if (source === "p2") {
+      setHideButtons(true);
+      setTimeout(() => setHideButtons(false), 3000);
       let conDMG = 0;
       if (stats1.hasCondition && stats1.condition === "leech") {
         const damageFraction = 1 / 16;
         conDMG = Math.floor(defenderStats.hp * damageFraction + 1);
       }
       if (p2HP >= pokemonSeed[2].stats.hp) {
-        setTimeout(() => setP2HP(pokemonSeed[2].stats.hp), 3000);
+        setTimeout(() => setP2HP(pokemonSeed[2].stats.hp), 2500);
       } else {
-        setTimeout(() => setP2HP(p2HP + conDMG), 3000);
+        setTimeout(() => setP2HP(p2HP + conDMG), 2500);
       }
-      setTimeout(() => setP1HP(hp - conDMG), 3000);
+      setTimeout(() => setP1HP(hp - conDMG), 2500);
       if (p1HP < 0) {
-        setTimeout(() => setP1HP(0), 3000);
+        setTimeout(() => setP1HP(0), 2500);
       }
       setLastMoveUsed(move);
       handleAddTurn(pokemonSeed[2].name, pokemonSeed[0].name, move);
-      setTimeout(() => setPlayerTurn(true), 4000);
+      setTimeout(() => setPlayerTurn(true), 3000);
     }
 
     console.log("move:", move); // do something with the move data
@@ -285,7 +302,7 @@ const BattleApp: React.FC<PokemonProps> = () => {
       {playerTurn ? (
         <div>
           <div>
-            <div style={{ position: "relative", left: "50px" }}>
+            <div style={{ position: "relative", left: "20px" }}>
               {pokemonSeed[2]?.name}
             </div>
             <div>
@@ -294,7 +311,7 @@ const BattleApp: React.FC<PokemonProps> = () => {
                 originalHP={pokemonSeed[2].stats.hp}
               />
               <img
-                style={{ position: "relative", left: "50px" }}
+                style={{ position: "relative", left: "70px" }}
                 src={pokemonSeed[2]?.image}
                 alt={pokemonSeed[2]?.name}
               />
@@ -304,12 +321,12 @@ const BattleApp: React.FC<PokemonProps> = () => {
             <animated.img
               style={{
                 position: "relative",
-                right: "0px",
+                right: "50px",
                 zIndex: 1,
                 width: "150px",
                 height: "150px",
 
-                ...springProps,
+                ...props,
               }}
               src={pokemonSeed[0]?.imageBack}
               alt={pokemonSeed[0]?.name}
@@ -322,14 +339,18 @@ const BattleApp: React.FC<PokemonProps> = () => {
                 currentHP={p1HP}
                 originalHP={pokemonSeed[0].stats.hp}
               />
-              {pokemonSeed[0]?.moves.slice(0, 4).map((move) => (
-                <button
-                  key={move.name}
-                  onClick={() => handleMoveClick(move, "p1")}
-                >
-                  {move.name}
-                </button>
-              ))}
+              {!hideButtons ? (
+                pokemonSeed[0]?.moves.slice(0, 4).map((move) => (
+                  <button
+                    key={move.name}
+                    onClick={() => handleMoveClick(move, "p1")}
+                  >
+                    {move.name}
+                  </button>
+                ))
+              ) : (
+                <></>
+              )}
             </div>
             <div>
               {lastMoveUsed ? (
@@ -354,7 +375,7 @@ const BattleApp: React.FC<PokemonProps> = () => {
                 originalHP={pokemonSeed[0].stats.hp}
               />
               <img
-                style={{ position: "relative", left: "50px" }}
+                style={{ position: "relative", left: "70px" }}
                 src={pokemonSeed[0]?.image}
                 alt={pokemonSeed[0]?.name}
               />
@@ -364,12 +385,12 @@ const BattleApp: React.FC<PokemonProps> = () => {
             <animated.img
               style={{
                 position: "relative",
-                right: "0px",
+                right: "50px",
                 zIndex: 1,
                 width: "150px",
                 height: "150px",
 
-                ...springProps,
+                ...props,
               }}
               src={pokemonSeed[2]?.imageBack}
               alt={pokemonSeed[2]?.name}
@@ -382,14 +403,18 @@ const BattleApp: React.FC<PokemonProps> = () => {
                 currentHP={p2HP}
                 originalHP={pokemonSeed[2].stats.hp}
               />
-              {pokemonSeed[2]?.moves.slice(0, 4).map((move) => (
-                <button
-                  key={move.name}
-                  onClick={() => handleMoveClick(move, "p2")}
-                >
-                  {move.name}
-                </button>
-              ))}
+              {!hideButtons ? (
+                pokemonSeed[2]?.moves.slice(0, 4).map((move) => (
+                  <button
+                    key={move.name}
+                    onClick={() => handleMoveClick(move, "p2")}
+                  >
+                    {move.name}
+                  </button>
+                ))
+              ) : (
+                <></>
+              )}
             </div>
             <div>
               {lastMoveUsed ? (
